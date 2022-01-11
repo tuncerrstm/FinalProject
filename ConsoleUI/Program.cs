a176115eaf4a3702407f4f3ba5222bdc81480fe0
tuncerrstm <tuncerrstm@gmail.com>
2022-01-11 17:19:55 +0300

    Bootcamp Day8 - C# Entity Framework

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    // SOLID 
+    // Bu yaptığımız hareket solidin O harfi : Yaptığımız yazılıma yeni bir özellik eliyorsak mevcuttaki hiç bir koda dokunamayız.
+    // Open Closed Principle bu anlama gelir.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+        }
+    }
+}
