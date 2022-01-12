78e6c3ea1b1c10fba16c57908ee0885b3e5ff636
tuncerrstm <tuncerrstm@gmail.com>
2022-01-12 21:00:05 +0300

    Bootcamp Day 9 - C# Enterprise Architecture

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    // Generic Constraint ( Genel Kısıtlama )
+    // class : referans tip olabilir anlamına gelmektedir.
+    // IEntity :IEntity olabilir veye IEntity implemente eden bir nesne olabilir.
+    // new() : new'lenebilir olmalıdır.
+    public interface IEntityRepository<T> where T:class, IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
