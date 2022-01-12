552ab8bf4ea213dc8208a25e8f1f99a825a9c8fd
tuncerrstm <tuncerrstm@gmail.com>
2022-01-12 23:00:32 +0300

    Bootcamp Day 9 - C# Enterprise Architecture : Code Refactoring

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            // Varsa: İş kodlarını buraya yazarız!
+            return _categoryDal.GetAll();
+        }
+
+        // Bu kod şunu çalıştırır:
+        // Select * from Categories where CategoryId = 3 ( <--- Verilen id 'ye göre category getirir.)
+        public Category GetById(int categoryId)
+        {
+            return _categoryDal.Get(c => c.CategoryId == categoryId);
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,13 +12,30 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // IoC Diye bir kavramla new ' lememize gerek olmayacak. Şuan hala code refactor edilmeli.
+            // DTO : Data Tranformation Object (Yani taşıyacağımız objeler anlamına gelir.)
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
-
         }
     }
 }
