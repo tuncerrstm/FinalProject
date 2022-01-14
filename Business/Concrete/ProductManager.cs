4021204d796ab3363aa0fcf232df72a322c3ad50
tuncerrstm <tuncerrstm@gmail.com>
2022-01-14 05:30:46 +0300

    Results : Code Refactoring

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -19,17 +21,44 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public List<Product> GetAll()
+        public IResult Add(Product product)
+        {
+            // Business Codes.
+            
+            if(product.ProductName.Length < 2)
+            {
+                // Magic Strings bunlardan kurtulmalıyız. Büyüyen projelerde sorun ve karmaşa haline gelir
+                // O yüzden Bussines katmanında standart hale getirir ve temiz bir kod yazarız. Constants klasörü oluşturulur.
+                // Constants : Sabitler anlamına gelir ve projenin sabitleri bu klasöre tanımlanır.
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+
+            _productDal.Add(product);
+
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        public IDataResult<List<Product>> GetAll() 
         {
             // İş Kodları Buraya Yazılır.
             // Yetkisi var mı? İzinlerin sorgulaması burada yapılır!
-            return _productDal.GetAll();
+            if (DateTime.Now.Hour == 22)
+            {
+                return new ErrorResult();
+            }
+
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),true,"Ürünler Listelendi");
 
         }
 
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll( p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages // static yapılar new lenmez. Sabit mesaj olduğu için bu tip yapıları static yaparız. 
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+    }
+}
